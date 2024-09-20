312f8c2351eb4dec0025b216ee1cef54639af907
TanishqDosaya <145388361+TanishqDosaya@users.noreply.github.com>
2024-09-20 13:11:18 +0530

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,12 @@
+public class Constant {
+    String PositiveBoard = "Enter board size (positive integer, e.g., 4):";
+    String PositiveSize = "Size must be a positive integer";
+    String Invalid = "Invalid input. Please enter a positive integer.";
+    String Error = "Error";
+    String Title = "2048 by @Tanishq";
+    String GameOver = "Game Over";
+    String Won = "You WON!";
+    String WonPlayAgain = "You WON! Play again?";
+    String Lost = "You LOST!";
+    String LostPlayAgain = "You LOST! Play again?";
+}
